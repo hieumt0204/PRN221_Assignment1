73a6ab84cafdaa39ab82e1d6d7171098bc3f10f7
hieumthe1638431 <hieumthe163841@fpt.edu.vn>
2024-01-15 13:02:35 +0700

    update author

--- Assigment1/OrderReportWindow.xaml.cs
+++ Assigment1/OrderReportWindow.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Session2.Models;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Assignment1
 {
@@ -32,17 +22,14 @@ namespace Assignment1
         {
             try
             {
-                // Lấy ngày bắt đầu và ngày kết thúc từ DatePicker
                 DateTime startDate = datePickerStartDate.SelectedDate ?? DateTime.MinValue;
                 DateTime endDate = datePickerEndDate.SelectedDate ?? DateTime.MaxValue;
 
-                // Lấy danh sách đơn đặt hàng theo khoảng thời gian
                 var orders = context.Orders
                     .Include(x => x.Member)
                     .Where(o => o.OrderDate >= startDate && o.OrderDate <= endDate)
                     .ToList();
 
-                // Gán danh sách đơn đặt hàng cho ItemsSource của ListView
                 lvOrders.ItemsSource = orders;
             }
             catch(Exception ex) 
